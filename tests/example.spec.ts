f24a21bec900ed893379dc45c0c3ebd467105c61
xuanhieu12345 <hieuxuan213@gmail.com>
2026-07-14 11:17:27 +0700

    change smoke to regression

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-// Gắn tag @smoke để khớp với lệnh --grep "@smoke" trong file CI của bạn
-test('Kịch bản test mẫu cho CI/CD @smoke', async ({ page }) => {
-    // Truy cập trang web
+// SỬA CHỮ @smoke THÀNH @regression
+test('Kịch bản test mẫu cho CI/CD @regression', async ({ page }) => {
     await page.goto('https://playwright.dev/');
-
-    // Kiểm tra tiêu đề trang
     await expect(page).toHaveTitle(/Playwright/);
-
 });
