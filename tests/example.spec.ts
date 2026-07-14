7b824166458a63370933140863f5ddb92622cc71
xuanhieu12345 <hieuxuan213@gmail.com>
2026-07-14 11:38:15 +0700

    change add base give project

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,24 @@
 import { test, expect } from '@playwright/test';
+import { customStep } from '../src/utils/reportHelper';
 
-// SỬA CHỮ @smoke THÀNH @regression
-test('Kịch bản test mẫu cho CI/CD @regression', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    await expect(page).toHaveTitle(/Playwright/);
+// 1. Kịch bản này sẽ chạy khi có PULL REQUEST hoặc CHẠY TAY chọn @smoke
+test('Kiểm tra trang chủ Playwright @smoke', async ({ page }) => {
+    await customStep(page, 'Truy cập Playwright', async () => {
+        await page.goto('https://playwright.dev/');
+    });
+
+    await customStep(page, 'Xác nhận tiêu đề', async () => {
+        await expect(page).toHaveTitle(/Playwright/);
+    });
+});
+
+// 2. Kịch bản này sẽ chạy khi PUSH thẳng lên Main, Schedule ban đêm, hoặc CHẠY TAY chọn @regression
+test('Kiểm tra trang chủ GitHub @regression', async ({ page }) => {
+    await customStep(page, 'Truy cập GitHub', async () => {
+        await page.goto('https://github.com/');
+    });
+
+    await customStep(page, 'Xác nhận tiêu đề', async () => {
+        await expect(page).toHaveTitle(/GitHub/);
+    });
 });
